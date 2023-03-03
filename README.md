81b9d78e80ba53c495d9a3ad80c7045a87858222
ned <github@edoardo.fyi>
2023-03-03 15:37:21 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+<img src="https://github.com/n3d1117/mastofact/files/10888534/mastofact.pdf" width="150" height="150">
+
+# Mastofact
+![python-version](https://img.shields.io/badge/python-3.10-blue.svg)
+[![openai-version](https://img.shields.io/badge/openai-0.27.0-green.svg)](https://openai.com/)
+[![mastodon.py-version](https://img.shields.io/badge/mastodon.py-1.8.0-red.svg)](https://openai.com/)
+[![license](https://img.shields.io/badge/License-MIT-brightgreen.svg)](LICENSE)
+
+A silly ChatGPT-powered Mastodon bot that toots a random fact every hour. Running at [@mastofact](https://mastodon.social/@mastofact)
+
+## Prerequisites
+- Python 3.10+ and [Pipenv](https://pipenv.readthedocs.io/en/latest/)
+- A Mastodon access token (create one under `Development` > `Your applications` > `New application` > `Your access token`)
+- An [OpenAI](https://openai.com) account (see [configuration](#configuration) section)
+
+## Getting started
+
+### Configuration
+Customize the configuration by copying `.env.example` and renaming it to `.env`, then editing the parameters as desired:
+```bash
+MASTODON_BOT_TOKEN="YOUR_MASTODON_BOT_TOKEN"
+OPENAI_API_KEY="YOUR_OPENAI_API_KEY"
+
+# Optional parameters
+POST_INTERVAL_SECONDS=3600 # Defaults to 3600 (1h)
+```
+
+### Installing
+1. Clone the repository and navigate to the project directory:
+
+```bash
+git clone https://github.com/n3d1117/mastofact.git
+cd mastofact
+```
+
+#### From Source
+2. Create a new virtual environment with Pipenv and install the required dependencies:
+```
+pipenv install
+```
+
+3. Activate the virtual environment:
+```
+pipenv shell
+```
+
+4. Use the following command to start the bot:
+```
+python main.py
+```
+
+#### Using Docker Compose
+
+2. Run the following command to build and run the Docker image:
+```bash
+docker-compose up
+```
+
+## Credits
+- [ChatGPT](https://chat.openai.com/chat) from [OpenAI](https://openai.com)
+- [Mastodon.py](https://github.com/halcy/Mastodon.py)
+
+## Disclaimer
+This is a personal project and is not affiliated with OpenAI in any way.
+
+## License
+MIT license. For more information, see the [LICENSE](LICENSE) file included in the repository.
---
+++ bot.py
@@ -0,0 +1,74 @@
+import os
+import time
+
+from mastodon import Mastodon
+import openai
+import logging
+from dotenv import load_dotenv
+
+
+def main():
+    # Read .env file
+    load_dotenv()
+
+    # Setup logging
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+
+    # Check if the required environment variables are set
+    required_values = ['MASTODON_BOT_TOKEN', 'OPENAI_API_KEY']
+    missing_values = [value for value in required_values if os.environ.get(value) is None]
+    if len(missing_values) > 0:
+        logging.error(f'The following environment values are missing in your .env: {", ".join(missing_values)}')
+        exit(1)
+
+    # Setup Mastodon
+    mastodon = Mastodon(
+        access_token=os.environ['MASTODON_BOT_TOKEN'],
+        api_base_url='https://mastodon.social/'
+    )
+
+    # Setup OpenAI
+    openai.api_key = os.environ['OPENAI_API_KEY']
+    prompt = "Tell me a random fact, be it fun, lesser-known or just interesting."
+    history = [{
+        "role": "system",
+        "content": "You are a helpful assistant. When asked about a random fun/lesser known fact, "
+                   "you never answer with the same fact twice. If don't know any more facts, "
+                   "start making them up. Before answering, always check your previous answers to "
+                   "make sure you haven't answered with the same fact before."
+    }]
+
+    # Post status every POST_INTERVAL_SECONDS
+    while True:
+        try:
+            history.append({"role": "user", "content": prompt})
+
+            # Only keep the last 10 messages to avoid excessive token usage
+            if len(history) > 10:
+                history = history[-10:]
+
+            response = openai.ChatCompletion.create(
+                model='gpt-3.5-turbo',
+                messages=history
+            )
+
+            answer = response.choices[0]['message']['content']\
+                .strip()\
+                .encode('utf-8')\
+                .decode('utf-8')
+
+            mastodon.status_post(status=answer)
+
+            logging.info(f'Posted status: "{answer}", usage: {response.usage.total_tokens} tokens')
+
+        except Exception as e:
+            logging.error(e)
+
+        time.sleep(float(os.environ.get('POST_INTERVAL_SECONDS', 3600)))
+
+
+if __name__ == '__main__':
+    main()
